e222084ad0dda81354f47a9e9f29f56b34348cc3
instereo4 <lpinstereo@gmail.com>
2022-05-05 14:43:50 -0300

    Succubus particles folder
    succubus particles skills 2, 3 and ult created

---
+++ game/scripts/vscripts/heroes/succubus/succubus__attributes.lua
@@ -0,0 +1,291 @@
+succubus__attributes = class ({})
+LinkLuaModifier("succubus__modifier_effect", "heroes/succubus/succubus__modifier_effect", LUA_MODIFIER_MOTION_NONE)
+require("talent_tree")
+
+--TALENT FUNCTIONS
+	succubus__attributes.talents = {
+		[1] = {
+			[0] = false, [11] = false, [12] = false, [13] = false, [21] = false, [22] = false,
+			[23] = false, [31] = false, [32] = false, [33] = false, [41] = false, [42] = false
+		},
+		[2] = {
+			[0] = false, [11] = false, [12] = false, [13] = false, [21] = false, [22] = false,
+			[23] = false, [31] = false, [32] = false, [33] = false, [41] = false, [42] = false
+		},
+		[3] = {
+			[0] = false, [11] = false, [12] = false, [13] = false, [21] = false, [22] = false,
+			[23] = false, [31] = false, [32] = false, [33] = false, [41] = false, [42] = false
+		},
+		[4] = {
+			[0] = false, [11] = false, [12] = false, [13] = false, [21] = false, [22] = false,
+			[23] = false, [31] = false, [32] = false, [33] = false, [41] = false, [42] = false
+		}
+	}
+
+	succubus__attributes.skills = {
+		[1] = "succubus_1__dash",
+		[2] = "succubus_2__heart",
+		[3] = "succubus_3__clone",
+		[4] = "succubus_u__bat"
+	}
+
+	function succubus__attributes:UpgradeRank(skill, id, level)
+		local caster = self:GetCaster()
+		self:CheckNewAbility(skill, id, level)
+
+		if skill == 5 then
+			if id == 0 then
+				self.stats_bonus = self.stats_bonus + 1
+				
+				local base_str = self:GetSpecialValueFor("base_STR") + self.stats_bonus
+				local skill_str = caster:FindAbilityByName("_1_STR"):SetBasePts(base_str)
+		
+				local base_agi = self:GetSpecialValueFor("base_AGI") + self.stats_bonus
+				local skill_agi = caster:FindAbilityByName("_1_AGI"):SetBasePts(base_agi)
+				
+				local base_int = self:GetSpecialValueFor("base_INT") + self.stats_bonus
+				local skill_int = caster:FindAbilityByName("_1_INT"):SetBasePts(base_int)
+
+				local base_con = self:GetSpecialValueFor("base_CON") + self.stats_bonus
+				local skill_con = caster:FindAbilityByName("_1_CON"):SetBasePts(base_con)
+
+				local void = caster:FindAbilityByName("_void")
+				if void then void:SetLevel(1) end
+
+				caster:AddExperience(10, 0, false, false)
+				return
+			end
+			self.extras_unlocked = self.extras_unlocked + 1
+			caster:AddExperience(level * 10, 0, false, false)
+			SendOverheadEventMessage(nil, OVERHEAD_ALERT_SHARD, caster, level, caster)
+			return
+		end
+
+		local ability = nil
+		if skill == 1 then ability = caster:FindAbilityByName("succubus_1__dash") end
+		if skill == 2 then ability = caster:FindAbilityByName("succubus_2__heart") end
+		if skill == 3 then ability = caster:FindAbilityByName("succubus_3__clone") end
+		if skill == 4 then ability = caster:FindAbilityByName("succubus_u__bat") end
+
+		self.talents[skill][id] = true
+
+		if not ability then return end
+		if not ability:IsTrained() then return end
+		ability:SetLevel(ability:GetLevel() + level)
+		caster:AddExperience(level * 10, 0, false, false)
+		SendOverheadEventMessage(nil, OVERHEAD_ALERT_SHARD, caster, level, caster)
+	end
+
+	function succubus__attributes:CheckNewAbility(skill, id, level)
+		local caster = self:GetCaster()
+
+		if skill == 5 and id == 1 then caster:FindAbilityByName("succubus_x1__extra1"):SetLevel(level) end
+		if skill == 5 and id == 2 then caster:FindAbilityByName("succubus_x2__extra2"):SetLevel(level) end
+	end
+
+--SHOW ATTRIBUTES FUNCTIONS
+	function succubus__attributes:GetIntrinsicModifierName()
+		return "succubus__modifier_effect"
+	end
+
+	function succubus__attributes:GetAbilityTextureName()
+		if self:GetToggleState() then
+			return "attributes_off"
+		else
+			return "attributes_on"
+		end
+	end
+
+	function succubus__attributes:OnToggle()
+		local caster = self:GetCaster()
+		local ATTs = {
+			[1] = caster:FindAbilityByName("_2_DEX"),
+			[2] = caster:FindAbilityByName("_2_DEF"),
+			[3] = caster:FindAbilityByName("_2_RES"),
+			[4] = caster:FindAbilityByName("_2_REC"),
+			[5] = caster:FindAbilityByName("_2_MND"),
+			[6] = caster:FindAbilityByName("_2_LCK"),
+		}
+
+		if self:GetToggleState() then
+			for _,att in pairs(ATTs) do
+				att:SetHidden(false)
+			end
+		else
+			for _,att in pairs(ATTs) do
+				att:SetHidden(true)
+			end
+		end
+	end
+
+--LEVELUP ATTRIBUTES FUNCTIONS
+	function succubus__attributes:Spawn()
+		local caster = self:GetCaster()
+		caster:SetAbilityPoints(1)
+		
+		Timers:CreateTimer((0.2), function()
+			if caster:IsIllusion() == false then
+				caster:Heal(1000, nil)
+				if self:IsTrained() then TalentTree:SetupForHero(caster) end
+			end
+		end)
+		
+		self:SetHidden(false)
+		self.extras_unlocked = 0
+		self.stats_bonus = 0
+		self.base_upgrade = false
+		
+		self.str = 0
+		self.agi = 0
+		self.int = 0
+		self.con = 0
+		self.basic_fraction_dex = 0
+		self.basic_fraction_def = 0
+		self.basic_fraction_res = 0
+		self.basic_fraction_rec = 0
+		self.basic_fraction_mnd = 0
+		self.basic_fraction_lck = 0
+
+		if self:IsTrained() == false then self:UpgradeAbility(true) end
+	end
+
+	function succubus__attributes:OnHeroLevelUp()
+		local caster = self:GetCaster()
+		local level = caster:GetLevel()
+		if caster:IsIllusion() then return end
+
+		local gain = 0
+		if level ~= 2 and level ~= 5 and level ~= 8 then gain = -1 end
+		if level > 8 then gain = gain + 3 end
+		if level > 12 then gain = gain + 1 end
+		if level > 16 then gain = gain + 1 end
+		if level == 8 then gain = 12 end
+
+		caster:SetAbilityPoints((caster:GetAbilityPoints() + gain))
+
+		self:UpgradeAbility(true)
+
+		if level == 24 then
+			for i = 1, 5, 1 do
+				self:UpgradeAbility(true)
+			end
+		end
+	end
+
+	function succubus__attributes:OnUpgrade()
+		local caster = self:GetCaster()
+
+		local skill_str = caster:FindAbilityByName("_1_STR")
+		local base_str = self:GetSpecialValueFor("base_STR")
+		local str = self:GetSpecialValueFor("STR")
+
+		local skill_agi = caster:FindAbilityByName("_1_AGI")
+		local base_agi = self:GetSpecialValueFor("base_AGI")
+		local agi = self:GetSpecialValueFor("AGI")
+
+		local skill_int = caster:FindAbilityByName("_1_INT")
+		local base_int = self:GetSpecialValueFor("base_INT")
+		local int = self:GetSpecialValueFor("INT")
+
+		local skill_con = caster:FindAbilityByName("_1_CON")
+		local base_con = self:GetSpecialValueFor("base_CON")
+		local con = self:GetSpecialValueFor("CON")
+
+		if self.base_upgrade == false then
+			self.base_upgrade = true
+			self.str = base_str
+			self.agi = base_agi
+			self.int = base_int
+			self.con = base_con
+
+			skill_str:SetBasePts(base_str)
+			skill_agi:SetBasePts(base_agi)
+			skill_int:SetBasePts(base_int)
+			skill_con:SetBasePts(base_con)
+		end
+
+		if caster:IsIllusion() then return end
+
+		self.str = self.str + str
+		str = math.floor(self.str)
+		self.agi = self.agi + agi
+		agi = math.floor(self.agi)
+		self.int = self.int + int
+		int = math.floor(self.int)
+		self.con = self.con + con
+		con = math.floor(self.con)
+
+		if str > 0 then
+			for x = 1, str, 1 do
+				skill_str:UpgradeAbility(true)
+				self.basic_fraction_lck = self.basic_fraction_lck + 1
+				self.basic_fraction_res = self.basic_fraction_res + 1
+				self.basic_fraction_def = self.basic_fraction_def + 1
+			end
+		end
+		
+		if agi > 0 then
+			for x = 1, agi, 1 do
+				skill_agi:UpgradeAbility(true)
+				self.basic_fraction_lck = self.basic_fraction_lck + 1
+				self.basic_fraction_dex = self.basic_fraction_dex + 1
+				self.basic_fraction_rec = self.basic_fraction_rec + 1
+			end
+		end
+
+		if int > 0 then
+			for x = 1, int, 1 do
+				skill_int:UpgradeAbility(true)
+				self.basic_fraction_res = self.basic_fraction_res + 1
+				self.basic_fraction_rec = self.basic_fraction_rec + 1
+				self.basic_fraction_mnd = self.basic_fraction_mnd + 1
+			end
+		end
+
+		if con > 0 then
+			for x = 1, con, 1 do
+				skill_con:UpgradeAbility(true)
+				self.basic_fraction_def = self.basic_fraction_def + 1
+				self.basic_fraction_mnd = self.basic_fraction_mnd + 1
+				self.basic_fraction_dex = self.basic_fraction_dex + 1
+			end
+		end
+
+		self:SetBasicFraction("_2_DEX", self.basic_fraction_dex)
+		self:SetBasicFraction("_2_DEF", self.basic_fraction_def)
+		self:SetBasicFraction("_2_RES", self.basic_fraction_res)
+		self:SetBasicFraction("_2_REC", self.basic_fraction_rec)
+		self:SetBasicFraction("_2_MND", self.basic_fraction_mnd)
+		self:SetBasicFraction("_2_LCK", self.basic_fraction_lck)
+
+		self.str = self.str % 1
+		self.agi = self.agi % 1
+		self.int = self.int % 1
+		self.con = self.con % 1
+		self.basic_fraction_dex = self.basic_fraction_dex % 2
+		self.basic_fraction_def = self.basic_fraction_def % 2
+		self.basic_fraction_res = self.basic_fraction_res % 2
+		self.basic_fraction_rec = self.basic_fraction_rec % 2
+		self.basic_fraction_mnd = self.basic_fraction_mnd % 2
+		self.basic_fraction_lck = self.basic_fraction_lck % 2
+	end
+
+	function succubus__attributes:SetBasicFraction(secondary_att, fraction)
+		local caster = self:GetCaster()
+		local sub = caster:FindAbilityByName(secondary_att)
+		if sub == nil then return end
+
+		local levels = math.floor(fraction / 2)
+		if levels > 0 then
+			for x = 1, levels, 1 do
+				sub:EnableBasicUpgrade()
+				sub:UpgradeAbility(true)
+			end
+		end
+	end
+
+--PRECACHE
+	function succubus__attributes:Precache(context)
+		PrecacheResource("soundfile", "soundevents/name.vsndevts", context)
+		PrecacheResource("particle", "path", context)
+	end
---
+++ game/scripts/vscripts/heroes/succubus/succubus_u__bat.lua
@@ -0,0 +1,101 @@
+succubus_u__bat = class({})
+LinkLuaModifier("succubus_u_modifier_bat", "heroes/succubus/succubus_u_modifier_bat", LUA_MODIFIER_MOTION_NONE)
+
+-- INIT
+
+    function succubus_u__bat:CalcStatus(duration, caster, target)
+        local time = duration
+        local caster_int = nil
+        local caster_mnd = nil
+        local target_res = nil
+
+        if caster ~= nil then
+            caster_int = caster:FindModifierByName("_1_INT_modifier")
+            caster_mnd = caster:FindModifierByName("_2_MND_modifier")
+        end
+
+        if target ~= nil then
+            target_res = target:FindModifierByName("_2_RES_modifier")
+        end
+
+        if caster == nil then
+            if target ~= nil then
+                if target_res then time = time * (1 - target_res:GetStatus()) end
+            end
+        else
+            if target == nil then
+                if caster_mnd then time = duration * (1 + caster_mnd:GetBuffAmp()) end
+            else
+                if caster:GetTeamNumber() == target:GetTeamNumber() then
+                    if caster_mnd then time = duration * (1 + caster_mnd:GetBuffAmp()) end
+                else
+                    if caster_int then time = duration * (1 + caster_int:GetDebuffTime()) end
+                    if target_res then time = time * (1 - target_res:GetStatus()) end
+                end
+            end
+        end
+
+        if time < 0 then time = 0 end
+        return time
+    end
+
+    function succubus_u__bat:AddBonus(string, target, const, percent, time)
+        local att = target:FindAbilityByName(string)
+        if att then att:BonusPts(self:GetCaster(), self, const, percent, time) end
+    end
+
+    function succubus_u__bat:RemoveBonus(string, target)
+        local stringFormat = string.format("%s_modifier_stack", string)
+        local mod = target:FindAllModifiersByName(stringFormat)
+        for _,modifier in pairs(mod) do
+            if modifier:GetAbility() == self then modifier:Destroy() end
+        end
+    end
+
+    function succubus_u__bat:GetRank(upgrade)
+        local caster = self:GetCaster()
+        if caster:IsIllusion() then return end
+        local att = caster:FindAbilityByName("succubus__attributes")
+        if not att then return end
+        if not att:IsTrained() then return end
+        if caster:GetUnitName() ~= "npc_dota_hero_queenofpain" then return end
+
+        return att.talents[4][upgrade]
+    end
+
+    function succubus_u__bat:OnUpgrade()
+        local caster = self:GetCaster()
+        if caster:IsIllusion() then return end
+        if caster:GetUnitName() ~= "npc_dota_hero_queenofpain" then return end
+
+        local att = caster:FindAbilityByName("succubus__attributes")
+        if att then
+            if att:IsTrained() then
+                att.talents[4][0] = true
+            end
+        end
+        
+        if self:GetLevel() == 1 then
+			caster:FindAbilityByName("_2_DEX"):CheckLevelUp(true)
+			caster:FindAbilityByName("_2_DEF"):CheckLevelUp(true)
+			caster:FindAbilityByName("_2_RES"):CheckLevelUp(true)
+			caster:FindAbilityByName("_2_REC"):CheckLevelUp(true)
+			caster:FindAbilityByName("_2_MND"):CheckLevelUp(true)
+			caster:FindAbilityByName("_2_LCK"):CheckLevelUp(true)
+		end
+
+        local charges = 1
+        self:SetCurrentAbilityCharges(charges)
+    end
+
+    function succubus_u__bat:Spawn()
+        self:SetCurrentAbilityCharges(0)
+    end
+
+-- SPELL START
+
+    function succubus_u__bat:OnSpellStart()
+        local caster = self:GetCaster()
+    end
+
+-- EFFECTS
