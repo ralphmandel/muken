f1fe08fa7b0e20c32344c1ab5062c1bdec5a407a
Ralph Mandel de Barros <ralphmandelbarros@gmail.com>
2022-05-13 16:40:37 -0300

    ancient cosmetic updated 2

--- game/scripts/vscripts/modifiers/_modifier_cosmetics.lua
+++ game/scripts/vscripts/modifiers/_modifier_cosmetics.lua
@@ -24,7 +24,9 @@ function _modifier_cosmetics:OnCreated( kv )
     self.parent = self:GetParent()
     self.ability = self:GetAbility()
 
+	self.invi = false
 	self.model = kv.model
+	self.parent:SetOriginalModel(self.model)
 
 	Timers:CreateTimer((0.2), function()
 		self.parent:FollowEntity(self.caster, true)
@@ -45,7 +47,8 @@ function _modifier_cosmetics:CheckState()
 		[MODIFIER_STATE_UNSELECTABLE] = true,
 		[MODIFIER_STATE_NO_HEALTH_BAR] = true,
 		[MODIFIER_STATE_OUT_OF_GAME] = true,
-		[MODIFIER_STATE_NO_UNIT_COLLISION] = true
+		[MODIFIER_STATE_NO_UNIT_COLLISION] = true,
+		[MODIFIER_STATE_INVISIBLE] = self.invi
 	}
 
 	return state
@@ -67,7 +70,11 @@ end
 
 function _modifier_cosmetics:OnStateChanged(keys)
 	if keys.unit ~= self.caster then return end
-	if self.caster:IsHexed() or self.caster:IsOutOfGame() then
+	
+	if self.caster:IsInvisible() then self.invi = true else self.invi = false end
+
+	if self.caster:IsHexed()
+	or self.caster:IsOutOfGame() then
 		self.parent:AddNoDraw()
 	else
 		self.parent:RemoveNoDraw()
@@ -78,11 +85,11 @@ end
 
 function _modifier_cosmetics:PlayEfxAmbient()
 	if self.model == "models/items/elder_titan/harness_of_the_soulforged_weapon/harness_of_the_soulforged_weapon.vmdl" then
-		local string = "particles/econ/items/elder_titan/elder_titan_fissured_soul/elder_titan_fissured_soul_weapon.vpcf"
-		local effect_cast = ParticleManager:CreateParticle(string, PATTACH_POINT_FOLLOW, self.parent)
-		ParticleManager:SetParticleControl(effect_cast, 0, self.parent:GetOrigin())
-		ParticleManager:SetParticleControlEnt(effect_cast, 0, self.parent, PATTACH_POINT_FOLLOW, "attach_weapon", Vector(0,0,0), true)
-		self:AddParticle(effect_cast, false, false, -1, false, false)
+		local string = "particles/ancient/ancient_weapon.vpcf"
+		self.ancient_mace = ParticleManager:CreateParticle(string, PATTACH_POINT_FOLLOW, self.parent)
+		ParticleManager:SetParticleControl(self.ancient_mace, 0, self.parent:GetOrigin())
+		ParticleManager:SetParticleControlEnt(self.ancient_mace, 0, self.parent, PATTACH_POINT_FOLLOW, "attach_weapon", Vector(0,0,0), true)
+		self:AddParticle(self.ancient_mace, false, false, -1, false, false)
 	end
 
 	if self.model == "models/items/elder_titan/elder_titan_immortal_back/elder_titan_immortal_back.vmdl" then
@@ -90,20 +97,6 @@ function _modifier_cosmetics:PlayEfxAmbient()
 		local effect_cast = ParticleManager:CreateParticle(string, PATTACH_POINT_FOLLOW, self.parent)
 		ParticleManager:SetParticleControl(effect_cast, 0, self.parent:GetOrigin())
 		ParticleManager:SetParticleControlEnt(effect_cast, 0, self.parent, PATTACH_POINT_FOLLOW, "attach_back", Vector(0,0,0), true)
-		ParticleManager:SetParticleControl(effect_cast, 1, self.parent:GetOrigin())
-		ParticleManager:SetParticleControlEnt(effect_cast, 1, self.parent, PATTACH_POINT_FOLLOW, "attach_back_top_left", Vector(0,0,0), true)
-		ParticleManager:SetParticleControl(effect_cast, 2, self.parent:GetOrigin())
-		ParticleManager:SetParticleControlEnt(effect_cast, 2, self.parent, PATTACH_POINT_FOLLOW, "attach_back_top_right", Vector(0,0,0), true)
-		ParticleManager:SetParticleControl(effect_cast, 3, self.parent:GetOrigin())
-		ParticleManager:SetParticleControlEnt(effect_cast, 3, self.parent, PATTACH_POINT_FOLLOW, "attach_back_bot_right", Vector(0,0,0), true)
-		ParticleManager:SetParticleControl(effect_cast, 4, self.parent:GetOrigin())
-		ParticleManager:SetParticleControlEnt(effect_cast, 4, self.parent, PATTACH_POINT_FOLLOW, "attach_back_bot_left", Vector(0,0,0), true)
-		ParticleManager:SetParticleControl(effect_cast, 5, self.parent:GetOrigin())
-		ParticleManager:SetParticleControlEnt(effect_cast, 5, self.parent, PATTACH_POINT_FOLLOW, "attach_back", Vector(0,0,0), true)
-		ParticleManager:SetParticleControl(effect_cast, 6, self.parent:GetOrigin())
-		ParticleManager:SetParticleControlEnt(effect_cast, 6, self.parent, PATTACH_POINT_FOLLOW, "attach_back", Vector(0,0,0), true)
-		ParticleManager:SetParticleControl(effect_cast, 7, self.parent:GetOrigin())
-		ParticleManager:SetParticleControlEnt(effect_cast, 7, self.parent, PATTACH_POINT_FOLLOW, "attach_back", Vector(0,0,0), true)
 		self:AddParticle(effect_cast, false, false, -1, false, false)
 	end
 
@@ -115,6 +108,7 @@ function _modifier_cosmetics:PlayEfxAmbient()
 		ParticleManager:SetParticleControlEnt(effect_cast, 0, self.parent, PATTACH_POINT_FOLLOW, "attach_head", Vector(0,0,0), true)
 		self:AddParticle(effect_cast, false, false, -1, false, false)
 	end
+
 	if self.model == "models/items/shadow_demon/sd_crown_of_the_nightworld_armor/sd_crown_of_the_nightworld_armor.vmdl" then
 		local string = "particles/econ/items/shadow_demon/sd_crown_nightworld/sd_crown_nightworld_armor.vpcf"
 		local effect_cast = ParticleManager:CreateParticle(string, PATTACH_POINT_FOLLOW, self.parent)
