bd4778c965adba1892da50fcebdc14f9256f769e
Ralph Mandel de Barros <ralphmandelbarros@gmail.com>
2023-08-30 17:23:10 -0300

    sounds

--- content/panorama/scripts/custom_game/ui_econ_set_preview.js
+++ content/panorama/scripts/custom_game/ui_econ_set_preview.js
@@ -7,9 +7,20 @@ function OnUpdateHeroSelection() {
   var hero = Game.GetLocalPlayerInfo().possible_hero_selection;
 
   if (hero == "pudge") {sound_name = "Bocuse"}
-  if (hero == "riki") {sound_name = "Icebreaker"}
-  if (hero == "elder_titan") {sound_name = "Ancient"}
+  if (hero == "muerta") {sound_name = "Lawbreaker"}
+  if (hero == "slark") {sound_name = "Fleaman"}
+  if (hero == "shadow_demon") {sound_name = "Bloodstained"}
+
   if (hero == "sniper") {sound_name = "Hunter"}
+  if (hero == "shadow_shaman") {sound_name = "Dasdingo"}
+
+  if (hero == "riki") {sound_name = "Icebreaker"}
+  if (hero == "drow_ranger") {sound_name = "Genuine"}
+
+  if (hero == "elder_titan") {sound_name = "Ancient"}
+  if (hero == "dawnbreaker") {sound_name = "Paladin"}
+  if (hero == "omniknight") {sound_name = "Templar"}
+  if (hero == "bristleback") {sound_name = "Baldur"}
 
   Game.EmitSound("JP." + sound_name);
 
